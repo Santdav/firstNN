8b85ecdd5b55fe575b26350dbba0abacfc798fc1
Sanda2 <santiagoaristimuno2005@gmail.com>
2026-02-05 17:26:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Nombre del proyecto
+
+datos iniciales
+
+EDA y limpieza
+- Imputar por moda educacionde papas 
+- ...
+
+explicacion del modelo y eso
+
+# grafos
+
+#TODO
---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('student_habits_performance.csv')
+
+
+if __name__ == "__main__":
+    pass
